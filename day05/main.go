0feb6e2887bbdcea2502d37567304b693277826f
Kevin Wang <wangkevin448@gmail.com>
2016-12-05 09:48:20 -0800

    goroutined day05

--- day05/main.go
+++ day05/main.go
@@ -33,32 +33,56 @@ func main() {
 	start := time.Now()
 
 	password := ""
-	i := 0
+	passChan := make(chan string, 8)
+	passDone := make(chan bool)
+	iterations := 0
 
-	for i = 0; len(password) < 8; i++ {
-		hash := fmt.Sprintf("%x", md5.Sum([]byte(input+strconv.Itoa(i))))
-		if checkHash(hash) {
-			password += string(hash[5])
-			fmt.Println(hash)
-		}
-	}
-
-	password2 := [8]byte{0, 0, 0, 0, 0, 0, 0, 0}
-	j := 0
-
-	for j = 0; checkPass(password2[:]); j++ {
-		hash := fmt.Sprintf("%x", md5.Sum([]byte(input+strconv.Itoa(j))))
-		if checkHash(hash) {
-			k := hash[5] - '0'
-			if k < 8 && password2[k] == 0 {
-				password2[k] = hash[6]
-				fmt.Println(hash)
+	go func() {
+		j := 0
+		i := 0
+		for i = 0; j < 8; i++ {
+			hash := fmt.Sprintf("%x", md5.Sum([]byte(input+strconv.Itoa(i))))
+			if checkHash(hash) {
+				passChan <- string(hash[5])
+				j++
 			}
 		}
-	}
+		iterations = i
+		close(passChan)
+	}()
 
-	fmt.Println(fmt.Sprintf("\npassword: %s in %d iterations", password, i))
-	fmt.Println(fmt.Sprintf("\npassword2: %c in %d iterations", password2, j))
+	go func() {
+		for i := range passChan {
+			password += i
+			fmt.Println(password)
+		}
+		passDone <- true
+	}()
+
+	password2 := [8]byte{0, 0, 0, 0, 0, 0, 0, 0}
+	pass2Done := make(chan bool)
+	iterations2 := 0
+	go func() {
+		i := 0
+		for i = 0; checkPass(password2[:]); i++ {
+			hash := fmt.Sprintf("%x", md5.Sum([]byte(input+strconv.Itoa(i))))
+			if checkHash(hash) {
+				k := hash[5] - '0'
+				if k < 8 && password2[k] == 0 {
+					password2[k] = hash[6]
+					fmt.Println(fmt.Sprintf("%c", password2))
+				}
+			}
+		}
+		iterations2 = i
+		pass2Done <- true
+	}()
+
+	<-passDone
+	<-pass2Done
+
+	fmt.Println(fmt.Sprintf("\npassword: %s in %d iterations", password, iterations))
+	fmt.Println(fmt.Sprintf("\npassword2: %c in %d iterations", password2, iterations2))
 
 	fmt.Println(fmt.Sprintf("\ntime elapsed: %s", time.Since(start)))
 }
