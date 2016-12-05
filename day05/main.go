b60500feab47ebfce93b4f30012ce3cf6246567e
Kevin Wang <wangkevin448@gmail.com>
2016-12-05 12:13:55 -0800

    day 5 pipeline

--- day05/main.go
+++ day05/main.go
@@ -4,21 +4,20 @@ import (
 	"crypto/md5"
 	"fmt"
 	"strconv"
+	"sync"
 	"time"
+)
+
+type (
+	Vec2 struct {
+		X byte
+		Y byte
+	}
 )
 
 const (
 	input = "cxdnnyjw"
 )
-
-func checkPass(pass []byte) bool {
-	for _, i := range pass {
-		if i == 0 {
-			return true
-		}
-	}
-	return false
-}
 
 func checkHash(hash string) bool {
 	for j := 0; j < 5; j++ {
@@ -29,52 +28,104 @@ func checkHash(hash string) bool {
 	return true
 }
 
+func numGen(done <-chan bool) <-chan int {
+	send := make(chan int, 128)
+	go func() {
+		defer close(send)
+		for i := 0; true; i++ {
+			select {
+			case <-done:
+				return
+			case send <- i:
+			}
+		}
+	}()
+	return send
+}
+
+func hashWorker(done <-chan bool, receive <-chan int, send chan<- Vec2) {
+	for i := range receive {
+		hash := fmt.Sprintf("%x", md5.Sum([]byte(input+strconv.Itoa(i))))
+		if checkHash(hash) {
+			select {
+			case <-done:
+				return
+			case send <- Vec2{hash[5], hash[6]}:
+			}
+		}
+	}
+}
+
+func multiSendVec(done <-chan bool, receive <-chan Vec2, send ...chan<- Vec2) {
+	for i := range receive {
+		for _, j := range send {
+			select {
+			case <-done:
+				return
+			case j <- i:
+			}
+		}
+	}
+}
+
+func checkPass(pass []byte) bool {
+	for _, i := range pass {
+		if i == 0 {
+			return true
+		}
+	}
+	return false
+}
+
 func main() {
 	start := time.Now()
 
-	password := ""
-	passDone := make(chan bool)
-	iterations := 0
+	done := make(chan bool)
 
+	n := numGen(done)
+	hashChan := make(chan Vec2, 128)
+	for i := 0; i < 4; i++ {
+		go hashWorker(done, n, hashChan)
+	}
+
+	passSend := make(chan Vec2, 128)
+	pass2Send := make(chan Vec2, 128)
+
+	go multiSendVec(done, hashChan, passSend, pass2Send)
+
+	var wg sync.WaitGroup
+
+	password := ""
+	wg.Add(1)
 	go func() {
+		defer wg.Done()
 		j := 0
-		i := 0
-		for i = 0; j < 8; i++ {
-			hash := fmt.Sprintf("%x", md5.Sum([]byte(input+strconv.Itoa(i))))
-			if checkHash(hash) {
-				password += string(hash[5])
-				fmt.Println(password)
-				j++
-			}
+		for j = 0; j < 8; j++ {
+			hash := <-passSend
+			password += string(hash.X)
+			fmt.Println(password)
 		}
-		iterations = i
-		passDone <- true
 	}()
 
 	password2 := [8]byte{0, 0, 0, 0, 0, 0, 0, 0}
-	pass2Done := make(chan bool)
-	iterations2 := 0
+	wg.Add(1)
 	go func() {
-		i := 0
-		for i = 0; checkPass(password2[:]); i++ {
-			hash := fmt.Sprintf("%x", md5.Sum([]byte(input+strconv.Itoa(i))))
-			if checkHash(hash) {
-				k := hash[5] - '0'
-				if k < 8 && password2[k] == 0 {
-					password2[k] = hash[6]
-					fmt.Println(fmt.Sprintf("%c", password2))
-				}
+		defer wg.Done()
+		for checkPass(password2[:]) {
+			hash := <-pass2Send
+			k := hash.X - '0'
+			if k < 8 && password2[k] == 0 {
+				password2[k] = hash.Y
+				fmt.Println(fmt.Sprintf("%c", password2))
 			}
 		}
-		iterations2 = i
-		pass2Done <- true
 	}()
 
-	<-passDone
-	<-pass2Done
+	wg.Wait()
+	close(done)
 
-	fmt.Println(fmt.Sprintf("\npassword: %s in %d iterations", password, iterations))
-	fmt.Println(fmt.Sprintf("\npassword2: %c in %d iterations", password2, iterations2))
+	fmt.Println(fmt.Sprintf("\npassword: %s", password))
+	fmt.Println(fmt.Sprintf("\npassword2: %c", password2))
 
 	fmt.Println(fmt.Sprintf("\ntime elapsed: %s", time.Since(start)))
 }
