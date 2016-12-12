a534832f14317198b8aee236cb128dbada93213f
Kevin Wang <wangkevin448@gmail.com>
2016-12-12 14:26:05 -0800

    day 11 state

--- day11/main.go
+++ day11/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"time"
 )
 
@@ -17,7 +18,16 @@ type (
 		elevator  int
 		pairs     []Pair
 	}
+
+	Index struct {
+		id   int
+		chip bool
+	}
 )
+
+func NewIndex(id int, chip bool) Index {
+	return Index{id, chip}
+}
 
 func NewPair(gen, chip int) Pair {
 	return Pair{
@@ -58,8 +68,51 @@ func (p *Pair) down(limit int, chip bool) (Pair, bool) {
 	}
 }
 
+func (s *State) copy() State {
+	newPairs := make([]Pair, len(s.pairs))
+	copy(newPairs, s.pairs)
+	return State{
+		cost:      s.cost,
+		heuristic: s.heuristic,
+		elevator:  s.elevator,
+		pairs:     newPairs,
+	}
+}
+
+func (s *State) inc_cost() {
+	s.cost++
+}
+
+func (s *State) calc_heuristic(target *State) {
+	k := 0
+	for n, i := range s.pairs {
+		chip := target.pairs[n].chip
+		gen := target.pairs[n].gen
+		k += int(math.Abs(float64(chip-i.chip))) + int(math.Abs(float64(gen-i.gen)))
+	}
+	s.heuristic = k
+}
+
+func (s *State) elevator_up(limit int) bool {
+	k := s.elevator + 1
+	if k >= limit {
+		return false
+	}
+	s.elevator = k
+	return true
+}
+
+func (s *State) elevator_down(limit int) bool {
+	k := s.elevator - 1
+	if k < limit {
+		return false
+	}
+	s.elevator = k
+	return true
+}
+
 func (s *State) up(limit, pairId int, chip bool) (State, bool) {
-	newState := *s
+	newState := s.copy()
 	pair, success := s.pairs[pairId].up(limit, chip)
 	if success {
 		newState.pairs[pairId] = pair
@@ -70,7 +123,7 @@ func (s *State) up(limit, pairId int, chip bool) (State, bool) {
 }
 
 func (s *State) down(limit, pairId int, chip bool) (State, bool) {
-	newState := *s
+	newState := s.copy()
 	pair, success := s.pairs[pairId].down(limit, chip)
 	if success {
 		newState.pairs[pairId] = pair
@@ -80,10 +133,54 @@ func (s *State) down(limit, pairId int, chip bool) (State, bool) {
 	}
 }
 
-func main() {
-	start := time.Now()
+func (s *State) nextStates(target *State) []State {
+	states := []State{}
 
-	init_state := State{
+	indicies := []Index{}
+	for n, i := range s.pairs {
+		if i.gen == s.elevator {
+			indicies = append(indicies, NewIndex(n, false))
+		}
+		if i.chip == s.elevator {
+			indicies = append(indicies, NewIndex(n, true))
+		}
+	}
+
+	for i := 0; i < len(indicies); i++ {
+		k := indicies[i]
+		next_up, success1 := s.up(4, k.id, k.chip)
+		if success1 {
+			next_up.elevator_up(4)
+			next_up.inc_cost()
+			next_up.calc_heuristic(target)
+			states = append(states, next_up)
+		}
+		next_down, success2 := s.down(0, k.id, k.chip)
+		if success2 {
+			next_down.elevator_down(0)
+			next_down.inc_cost()
+			next_down.calc_heuristic(target)
+			states = append(states, next_down)
+		}
+		for j := i + 1; j < len(indicies); j++ {
+			l := indicies[j]
+			if success1 {
+				next, _ := next_up.up(4, l.id, l.chip)
+				next.calc_heuristic(target)
+				states = append(states, next)
+			}
+			if success2 {
+				next, _ := next_up.down(0, l.id, l.chip)
+				next.calc_heuristic(target)
+				states = append(states, next)
+			}
+		}
+	}
+	return states
+}
+
+var (
+	init_state = State{
 		cost:      0,
 		heuristic: 0,
 		elevator:  0,
@@ -96,7 +193,7 @@ func main() {
 		},
 	}
 
-	target_state := State{
+	target_state = State{
 		cost:      0,
 		heuristic: 0,
 		elevator:  3,
@@ -107,6 +204,18 @@ func main() {
 			NewPair(3, 3),
 			NewPair(3, 3),
 		},
+	}
+)
+
+func main() {
+	start := time.Now()
+
+	init_state.calc_heuristic(&target_state)
+
+	nextStates := init_state.nextStates(&target_state)
+
+	for _, i := range nextStates {
+		fmt.Println(i)
 	}
 
 	fmt.Println(fmt.Sprintf("time elapsed: %s", time.Since(start)))
